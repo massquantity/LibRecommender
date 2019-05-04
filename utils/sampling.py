a7c07b45ca93f195890ab5c1d5ad8df9a7532469
massquantity <wdmjjxg@163.com>
2019-05-04 18:35:46 +0800

    pairwise sampling

--- utils/sampling.py
+++ utils/sampling.py
@@ -37,9 +37,9 @@ class negative_sampling:
             label.append(label_indices[i])
             timestamp.append(timestamp_indices[i])
             for _ in range(self.num_neg):
-                item_neg = np.random.randint(0, self.dataset.n_items - 1)
+                item_neg = np.random.randint(0, self.dataset.n_items)
                 while item_neg in self.dataset.train_user[u]:
-                    item_neg = np.random.randint(0, self.dataset.n_items - 1)
+                    item_neg = np.random.randint(0, self.dataset.n_items)
 
                 user.append(u)
                 item.append(item_neg)
@@ -126,17 +126,16 @@ class negative_sampling:
         batch_pos_label = self.dataset.train_labels[self.i * self.batch_size: end]
 
         batch_user_indices, batch_item_indices, batch_label_indices = [], [], []
-        t0 = time.time()
         for i, u in enumerate(batch_pos_user):
             batch_user_indices.append(batch_pos_user[i])
             batch_item_indices.append(batch_pos_item[i])
             batch_label_indices.append(batch_pos_label[i])
 
             for _ in range(self.num_neg):
-                item_neg = np.random.randint(0, self.dataset.n_items - 1)
+                item_neg = np.random.randint(0, self.dataset.n_items)
         #        while item_neg in self.dataset.train_user[u]:
                 while self.dataset.train_user[u].__contains__(item_neg):
-                    item_neg = np.random.randint(0, self.dataset.n_items - 1)  # resample
+                    item_neg = np.random.randint(0, self.dataset.n_items)  # resample
 
                 batch_user_indices.append(u)
                 batch_item_indices.append(item_neg)
@@ -148,6 +147,106 @@ class negative_sampling:
         return np.array(batch_user_indices)[indices], \
                np.array(batch_item_indices)[indices], \
                np.array(batch_label_indices)[indices]
+
+
+class pairwise_sampling:
+    def __init__(self, dataset, batch_size=None):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.i = 0
+
+    def __call__(self, mode):
+        if mode == "train":
+            user_indices = self.dataset.train_user_indices
+            item_indices = self.dataset.train_item_indices
+            label_indices = self.dataset.train_labels
+        elif mode == "test":
+            user_indices = self.dataset.test_user_indices
+            item_indices = self.dataset.test_item_indices
+            label_indices = self.dataset.test_labels
+
+        user, item, label = [], [], []
+        for i, u in enumerate(user_indices):
+            user.append(user_indices[i])
+            item.append(item_indices[i])
+            label.append(label_indices[i])
+
+            item_neg = np.random.randint(0, self.dataset.n_items)
+            while item_neg in self.dataset.train_user[u]:
+                item_neg = np.random.randint(0, self.dataset.n_items)
+            user.append(u)
+            item.append(item_neg)
+            label.append(0.0)
+        return np.array(user), \
+               np.array(item), \
+               np.array(label)
+
+    def next_mf(self, user_factors, item_factors, bootstrap=False):
+        if bootstrap:
+            random_i = np.random.choice(len(self.dataset.train_user_indices), 1, replace=True)
+        #    random_i = np.random.randint(0, self.dataset.n_users)
+            user, item_i = self.dataset.train_user_indices[random_i][0], \
+                           self.dataset.train_item_indices[random_i][0]
+            item_j = np.random.randint(0, self.dataset.n_items)  # self.dataset.n_items - 1
+            while item_j in self.dataset.train_user[user]:
+                item_j = np.random.randint(0, self.dataset.n_items)
+
+            x_ui = np.dot(user_factors[user], item_factors[item_i])
+            x_uj = np.dot(user_factors[user], item_factors[item_j])
+            x_uij = x_ui - x_uj
+            return user, item_i, item_j, x_uij
+
+        elif self.batch_size == 1:
+            user, item_i = self.dataset.train_user_indices[self.i], \
+                           self.dataset.train_item_indices[self.i]
+            item_j = np.random.randint(0, self.dataset.n_items)
+            while item_j in self.dataset.train_user[user]:
+                item_j = np.random.randint(0, self.dataset.n_items)
+
+            x_ui = np.dot(user_factors[user], item_factors[item_i])
+            x_uj = np.dot(user_factors[user], item_factors[item_j])
+            x_uij = x_ui - x_uj
+            self.i += 1
+            return user, item_i, item_j, x_uij
+
+        elif self.batch_size > 1:
+            batch_item_j, batch_x_uij = [], []
+            end = min(len(self.dataset.train_user_indices), (self.i + 1) * self.batch_size)
+            batch_user = self.dataset.train_user_indices[self.i * self.batch_size: end]
+            batch_item_i = self.dataset.train_item_indices[self.i * self.batch_size: end]
+            for user, item_i in zip(batch_user, batch_item_i):
+                item_j = np.random.randint(0, self.dataset.n_items)
+                while item_j in self.dataset.train_user[user]:
+                    item_j = np.random.randint(0, self.dataset.n_items)
+                batch_item_j.append(item_j)
+
+                x_ui = np.dot(user_factors[user], item_factors[item_i])
+                x_uj = np.dot(user_factors[user], item_factors[item_j])
+                x_uij = x_ui - x_uj
+                batch_x_uij.append(x_uij)
+            self.i += 1
+            return batch_user, batch_item_i, np.array(batch_item_j), np.array(batch_x_uij)
+
+        else:
+            raise ValueError("either use bootstrap or batch size must be positive integer.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
