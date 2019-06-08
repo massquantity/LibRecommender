eedf56d87c42f3496555c24e7f6f8f116b4d40ec
massquantity <wdmjjxg@163.com>
2019-06-08 22:32:05 +0800

    test

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     t0 = time.time()
 #    dataset = DatasetPure()
 #    dataset.build_dataset(data_path="ml-1m/ratings.dat", sep="::", length="all", shuffle=True,
-#                          convert_implicit=True, build_negative=True, num_neg=1, batch_size=256)
+#                          convert_implicit=True, build_negative=True, num_neg=4, batch_size=256)
 #    dataset.leave_k_out_split(4, data_path="ml-1m/ratings.dat", length=100000, sep="::",
 #                              convert_implicit=True, build_negative=True, batch_size=256, num_neg=1)
 #    print("data size: ", len(dataset.train_user_implicit) + len(dataset.test_user_implicit))
@@ -27,14 +27,14 @@ if __name__ == "__main__":
 
     dataset = DatasetFeat(include_features=True)
     dataset.build_dataset("ml-1m/merged_data.csv", length=100000, user_col=0, item_col=1, label_col=2,
-                          numerical_col=None, categorical_col=[3, 4, 5], merged_categorical_col=[[6, 7, 8]],
+                          numerical_col=None, categorical_col=None, merged_categorical_col=None,
                           convert_implicit=True, build_negative=True, num_neg=1, batch_size=256)
 #                         numerical_col=None, categorical_col=[3, 4, 5, 6, 7, 8], merged_categorical_col=None)
 #    dataset.leave_k_out_split(4, data_path="ml-1m/merged_data.csv", length="all", sep=",", shuffle=True,
 #                              user_col=0, item_col=1, label_col=2, numerical_col=None, categorical_col=[3, 4, 5],
 #                              merged_categorical_col=[[6, 7, 8]])
     print("data size: ", len(dataset.train_indices_implicit) + len(dataset.test_indices_implicit))
-#    print("data processing time: {:.2f}".format(time.time() - t0))
+    print("data processing time: {:.2f}".format(time.time() - t0))
     print()
 #    dataset.build_trainset_implicit(4)
 #    dataset.build_testset_implicit(4)
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     # reg=0.001, n_factors=32 reg=0.0001   0.8586  0.8515  0.8511
     # reg=0.0003, n_factors=64, 0.8488    0.8471 0.8453
 #    fm = FM.FmPure(lr=0.001, n_epochs=20000, reg=0.0, n_factors=16, batch_size=256, task="ranking")  # orig 0.8650  0.8634 0.8591
-    fm = FM.FmFeat(lr=0.0001, n_epochs=20000, reg=0.0, n_factors=16, batch_size=256, task="ranking")
+    fm = FM.FmFeat(lr=0.001, n_epochs=20000, reg=0.0, n_factors=16, batch_size=256, task="ranking")
     fm.fit(dataset)
 #    print(fm.predict(1, 2))
 
