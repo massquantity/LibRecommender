d114a2597d38e076ba442092179805108c3c1995
massquantity <wdmjjxg@163.com>
2019-06-17 14:25:01 +0800

    Base

--- algorithms/Base.py
+++ algorithms/Base.py
@@ -1,5 +1,21 @@
-class Base:
-    pass
+from abc import ABCMeta, abstractmethod
 
 
+class Base(object):
+    __metaclass__ = ABCMeta
 
+    @abstractmethod
+    def build_model(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def fit(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def export_model(self):
+        raise NotImplementedError
