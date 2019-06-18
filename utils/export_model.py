fff2746ee740d1caead438e4c4aae02aecc0f6d2
massquantity <wdmjjxg@163.com>
2019-06-18 14:34:50 +0800

    export model

---
+++ utils/export_model.py
@@ -0,0 +1,10 @@
+import pickle
+import tensorflow as tf
+
+def export_model():
+    pickle.dump(...)
+
+
+def export_model_tf():
+    export_path = "."
+    builder = tf.saved_model.builder.SavedModelBuilder(export_path)
