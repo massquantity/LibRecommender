17e92323c321f7384212d4c27cde4c048699fdb2
massquantity <wdmjjxg@163.com>
2019-06-20 19:08:35 +0800

    similarities

--- utils/similarities.py
+++ utils/similarities.py
@@ -1,8 +1,11 @@
+import time
 from collections import defaultdict
 from multiprocessing import Pool
 import numpy as np
+from scipy.sparse import csr_matrix, issparse
 from math import sqrt
 import itertools
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def cosine_sim(dicts, x1, x2, min_support=5):
@@ -95,6 +98,76 @@ def get_sim_parallel(data, sim_func, i, ids, n_jobs=4, min_support=5):
         sim = np.array(sim)
     return sim
 
+
+def invert_sim(data, n_users, min_support=5):
+    prods = np.zeros((n_users, n_users))
+    num = np.zeros((n_users, n_users))
+    denom1 = np.zeros((n_users, n_users))
+    denom2 = np.zeros((n_users, n_users))
+    sim = np.zeros((n_users, n_users))
+
+    t0 = time.time()
+    for i, u_labels in data.items():
+        start_time = time.time()
+        for ui, li in u_labels.items():
+            for uj, lj in u_labels.items():
+                num[ui, uj] += 1
+                prods[ui, uj] += li * lj
+                denom1[ui, uj] += li * li
+                denom2[ui, uj] += lj * lj
+        print("item time: {:.2f}".format(time.time() - start_time))
+    print("time1: ", time.time() - t0)
+
+
+    t1 = time.time()
+    for ui in range(n_users):
+        sim[ui, ui] = 1.0
+        for uj in range(ui + 1, n_users):
+            if num[ui, uj] < min_support:
+                sim[ui, uj] = 0.0
+            else:
+                denom = np.sqrt(denom1[ui, uj] * denom2[ui, uj])
+                try:
+                    sim[ui, uj] = prods[ui, uj] / denom
+                except ZeroDivisionError:
+                    sim[ui, uj] = 0.0
+            sim[uj, ui] = sim[ui, uj]
+    print("time2: ", time.time() - t1)
+    return sim
+
+
+def sk_sim(data, n_users, n_items, min_support=5, sparse=True):
+    if sparse:
+        user_indices = []
+        item_indices = []
+        values = []
+        for i, u_ratings in data.items():
+            for u, r in u_ratings.items():
+                user_indices.append(u)
+                item_indices.append(i)
+                values.append(r)
+        m = csr_matrix((np.array(values), (np.array(user_indices), np.array(item_indices))))
+        assert issparse(m)
+    else:
+        m = np.zeros((n_users, n_items))
+        for i, u_ratings in data.items():
+            for u, r in u_ratings.items():
+                m[u, i] = r
+
+    sim = cosine_similarity(m)
+
+    if min_support > 0:
+        num = np.zeros((n_users, n_users))
+        for i, u_labels in data.items():
+            for ui, li in u_labels.items():
+                for uj, lj in u_labels.items():
+                    num[ui, uj] += 1
+
+        indices = np.where(num < min_support)
+        for i, j in zip(indices[0], indices[1]):
+            sim[i, j] = 0.0
+
+    return sim
 
 
 
