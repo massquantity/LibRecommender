3952ff8d11575b928d3c7feca4faf4c8652e4770
massquantity <wdmjjxg@163.com>
2019-06-27 06:54:22 +0800

    deploy_FM_request

---
+++ serving/deploy_FM_request.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import argparse
+import sys, os
+from pathlib import Path
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="request")
+    parser.add_argument("--data", "-d", type=str, help="sample data")
+    parser.add_argument("--host", default="localhost", help="host address")
+    parser.add_argument("--port", default=5000, help="port")
+    parser.add_argument("--algo", default=None, type=str, help="specific algorithm")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()         # http://127.0.0.1:5000/predict
+    url = "http://{host}:{port}/{algo}".format(host=args.host, port=args.port, algo=args.algo)
+    data_dict = json.loads(args.data)
+    data = {"user": [data_dict["user"]],
+            "item": [data_dict["item"]],
+            "sex": [data_dict["sex"]],
+            "occupation": [data_dict["occupation"]],
+            "title": [data_dict["title"]],
+            "genre1": [data_dict["genre1"]],
+            "genre2": [data_dict["genre2"]],
+            "genre3": [data_dict["genre3"]],
+        }
+
+    try:
+        response = requests.post(url, data=json.dumps(data))   #   json={'u': [1], 'i': [3]}
+        print(response.json())
+    except json.decoder.JSONDecodeError:
+        print("Oh noooooooooooooo, you should choose algorithm wisely...")
+
+#  python deploy_request.py --user 1 --item 3 -k 10 -n 10
+
+#  python deploy_FM_request.py --data '{"user": 1, "item": 1193, "sex": "F", "age": 1,
+#  "occupation": 10, "title": 2452.0, "genre1": "Drama", "genre2": "missing", "genre3": "missing"}' --algo FM
