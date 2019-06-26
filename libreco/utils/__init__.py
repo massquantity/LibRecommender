73f2ac5cd178b2a44b614db70b2a22b795f82c89
massquantity <wdmjjxg@163.com>
2019-06-27 06:51:33 +0800

    utils

--- libreco/utils/__init__.py
+++ libreco/utils/__init__.py
@@ -1,3 +1,3 @@
 from .baseline_estimates import baseline_als, baseline_sgd
 from .sampling import NegativeSampling
-from .export_model import export_model_pickle, export_model_joblib, export_model_tf
+from .export_model import export_model_pickle, export_model_joblib, export_model_tf, export_feature_transform
