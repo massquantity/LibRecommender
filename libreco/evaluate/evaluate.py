d79ae4de93284a4d994a277de16599a2f69eba43
massquantity <wdmjjxg@163.com>
2019-07-08 17:51:12 +0800

    evaluate

--- libreco/evaluate/evaluate.py
+++ libreco/evaluate/evaluate.py
@@ -3,39 +3,21 @@ import tensorflow as tf
 from sklearn.metrics import precision_score
 
 
-def rmse_knn(model, dataset, mode="train"):
+def rmse(model, dataset, mode="train"):
     if mode == "train":
         user_indices = dataset.train_user_indices
         item_indices = dataset.train_item_indices
-        ratings = dataset.train_labels
+        labels = dataset.train_labels
     elif mode == "test":
         user_indices = dataset.test_user_indices
         item_indices = dataset.test_item_indices
-        ratings = dataset.test_labels
+        labels = dataset.test_labels
 
     pred = []
     for j, (u, i) in enumerate(zip(user_indices, item_indices)):
         p = model.predict(u, i)
         pred.append(p)
-    score = np.sqrt(np.mean(np.power(pred - ratings, 2)))
-    return score
-
-
-def rmse_svd(model, dataset, baseline=False, mode="train"):
-    if mode == "train":
-        user_indices = dataset.train_user_indices
-        item_indices = dataset.train_item_indices
-        ratings = dataset.train_ratings
-    elif mode == "test":
-        user_indices = dataset.test_user_indices
-        item_indices = dataset.test_item_indices
-        ratings = dataset.test_ratings
-
-    pred = []
-    for u, i in zip(user_indices, item_indices):
-        p = model.predict(u, i)
-        pred.append(p)
-    score = np.sqrt(np.mean(np.power(pred - ratings, 2)))
+    score = np.sqrt(np.mean(np.power(pred - labels, 2)))
     return score
 
 
