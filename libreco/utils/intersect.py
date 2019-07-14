875275316ed298499d9bcfd77436f4d84585510c
massquantity <wdmjjxg@163.com>
2019-07-14 18:21:06 +0800

    intersect

--- libreco/utils/intersect.py
+++ libreco/utils/intersect.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import pickle
 from .similarities import *
 from .baseline_estimates import baseline_als
@@ -9,7 +10,8 @@ def get_intersect(dataset, sim_option="pearson", min_support=1, k=40, load=False
     n = len(dataset.train_item)
     ids = list(dataset.train_item.keys())
     if load:
-        sim_matrix = pickle.load(open("test/sim_matrix.pkl", "rb"))
+        with open(os.path.expanduser("~/Workspace/LibRecommender/sim.pkl"), "rb") as f:
+            sim_matrix = pickle.load(f)
     elif parallel:
         sim_matrix = get_sim_parallel(dataset.train_item, sim_option,
                                       ids, ids, min_support=min_support)
@@ -21,15 +23,24 @@ def get_intersect(dataset, sim_option="pearson", min_support=1, k=40, load=False
     for i in range(dataset.n_items):
         sim_whole[i] = np.argsort(sim_matrix[i])[::-1][:k]
 
-    intersect_user_item_train = {}
+#    intersect_user_item_train = {}
+    intersect_items_all = []
+    intersect_indices_all = []
+    u_labels_all = []
+#    base_neighbor_all = []
     for u, i in zip(dataset.train_user_indices, dataset.train_item_indices):
         u_items = list(dataset.train_user[u].keys())
         sim_items = sim_whole[i]
         intersect_items, index_u, _ = np.intersect1d(
             u_items, sim_items, assume_unique=True, return_indices=True)
-        intersect_user_item_train[(u, i)] = (intersect_items, index_u)
-
-    return intersect_user_item_train
+    #    intersect_user_item_train[(u, i)] = (intersect_items, index_u)
+        intersect_items_all.append(list(intersect_items))
+        intersect_indices_all.append(list(index_u))
+        u_labels_all.append(list(np.array(list(dataset.train_user[u].values()))[index_u]))
+#        base_neighbor = dataset.global_mean + baseline_user[u] + baseline_item[intersect_items]
+#        base_neighbor_all.append(list(base_neighbor))
+#    return intersect_user_item_train
+    return intersect_items_all, intersect_indices_all, u_labels_all
 
 
 def get_intersect_tf(dataset, sim_option="pearson", min_support=1, k=40, load=False, parallel=False):
