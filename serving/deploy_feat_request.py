0757f361e7c45a3fb966ef1096d2219c876a93fe
massquantity <wdmjjxg@163.com>
2019-10-07 19:43:32 +0800

    deploy

--- serving/deploy_feat_request.py
+++ serving/deploy_feat_request.py
@@ -17,16 +17,17 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()         # http://127.0.0.1:5000/predict
     url = "http://{host}:{port}/{algo}".format(host=args.host, port=args.port, algo=args.algo)
+#    url = "http://{host}/{algo}".format(host=args.host,  algo=args.algo)
     # if os.path.isfile(args.data)  elif isinstance(args.data, str)
     data_dict = json.loads(args.data)
-    data = {"user": [data_dict["user"]],
-            "item": [data_dict["item"]],
-            "sex": [data_dict["sex"]],
-            "occupation": [data_dict["occupation"]],
-            "title": [data_dict["title"]],
-            "genre1": [data_dict["genre1"]],
-            "genre2": [data_dict["genre2"]],
-            "genre3": [data_dict["genre3"]],
+    data = {"user": data_dict["user"],
+            "item": data_dict["item"],
+            "sex": data_dict["sex"],
+            "occupation": data_dict["occupation"],
+            "title": data_dict["title"],
+            "genre1": data_dict["genre1"],
+            "genre2": data_dict["genre2"],
+            "genre3": data_dict["genre3"],
         }
 
     try:
@@ -35,7 +36,10 @@ if __name__ == "__main__":
     except json.decoder.JSONDecodeError:
         print("Oh noooooooooooooo, you should choose algorithm wisely...")
 
-#  python deploy_pure_request.py --user 1 --item 3 -k 10 -n 10
 
-#  python deploy_feat_request.py --data '{"user": 1, "item": 1193, "sex": "F", "age": 1,
-#  "occupation": 10, "title": 2452.0, "genre1": "Drama", "genre2": "missing", "genre3": "missing"}' --algo FM
+#  python deploy_feat_request.py --data '{"user": [1], "item": [1193], "sex": ["F"], "age": [1],
+#  "occupation": [10], "title": [2452.0], "genre1": ["Drama"], "genre2": ["missing"], "genre3": ["missing"]}' --algo FM
+
+# python deploy_feat_request.py --data '{"user": [1, 1], "item": [1193, 1193], "sex": ["F", "F"], "age": [1, 1],
+# "occupation": [10, 10], "title": [2452.0, 2452.0], "genre1": ["Drama", "Drama"], "genre2": ["missing", "missing"],
+# "genre3": ["missing", "missing"]}' --algo FM
