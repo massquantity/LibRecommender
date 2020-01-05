d8eaeba887c1df788e05798b1c83f614e0c46f8f
massquantity <wdmjjxg@163.com>
2020-01-05 11:09:12 +0800

    spark

---
+++ serving/spark/src/main/com/libreco/TestJava.java
@@ -0,0 +1,7 @@
+package libreco;
+
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("Java completed...");
+    }
+}
---
+++ serving/spark/src/main/com/libreco/TestScala.scala
@@ -0,0 +1,7 @@
+package libreco
+
+object TestScala {
+  def main(args: Array[String]) = {
+    println("Scala completed...")
+  }
+}
