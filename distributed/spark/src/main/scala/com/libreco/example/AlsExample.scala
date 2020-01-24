e121bf380fbf09d26f4acb120f86e7a65689c9a5
massquantity <wdmjjxg@163.com>
2020-01-24 16:35:51 +0800

    Als

--- distributed/spark/src/main/scala/com/libreco/example/AlsExample.scala
+++ distributed/spark/src/main/scala/com/libreco/example/AlsExample.scala
@@ -1,20 +1,18 @@
 package com.libreco.example
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{Column, Dataset, Row, SparkSession}
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.ml.recommendation.{ALS, ALSModel}
 import org.apache.spark.sql.functions.{round, sum, lit}
-import com.libreco.utils.Context
+import com.libreco.utils.{Context, ItemNameConverter}
 import com.libreco.data.DataSplitter
 import com.libreco.model.Recommender
 
 import scala.util.Random
 
-object AlsExample extends Context{
+
+object AlsExample extends Context {
   import spark.implicits._
 
   def main(args: Array[String]): Unit = {
+
     val dataPath = this.getClass.getResource("/ml-1m/ratings.dat").toString
     val splitter = new DataSplitter()
     var data = spark.read.textFile(dataPath)
@@ -27,13 +25,17 @@ object AlsExample extends Context{
   //      .withColumn("percent", round($"count" / sum("count").over(), 4)).show()
 
     data = data.sample(withReplacement = false, 0.1)   // sample 0.1
-    val Array(train, test) = splitter.stratified_chrono_split(data, 0.8, "user")
+
     val model = new Recommender()
     time(model.train(data), "Training")
     val transformedData = model.transform(data)
     transformedData.show(4, truncate = false)
-    val rec = model.recommendForUsers(test, 10)
+
+    val movieMap = ItemNameConverter.getId2ItemName()
+    val rec = model.recommendForUsers(data, 10, movieMap)
     rec.show(20, truncate = false)
 
+  //  val model = new Recommender()
+  //  time(model.train(data, evaluate = true, num = 10), "Evaluating")
   }
 }
