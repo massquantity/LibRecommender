b2c299aa6350890a5ef7df27d2cf962258deebfa
massquantity <wdmjjxg@163.com>
2020-05-29 21:59:44 +0800

    sampling

--- distributed/new_features/utils/samplingNEW.py
+++ distributed/new_features/utils/samplingNEW.py
@@ -1,8 +1,6 @@
 import time
 import math
 import random
-from collections import defaultdict
-from multiprocessing import Pool
 import numpy as np
 
 
@@ -13,7 +11,24 @@ class SamplingBase(object):
         self.num_neg = num_neg
         self.batch_size = batch_size
 
-    def sample_items(self, seed=42):
+    def __call__(self, seed=42, dense=True, shuffle=False, mode="random"):
+        if mode not in ["random", "popular"]:
+            raise ValueError("sampling mode must either be 'random' or 'popular'")
+        else:
+            return self.random_sampling(seed, dense, shuffle, mode)
+
+    def _shuffle_data(self, dense, sparse_indices_sampled, dense_indices_sampled,
+                      dense_values_sampled, label_sampled, seed=42):
+        np.random.seed(seed)
+        total_length = len(self.dataset) * (self.num_neg + 1)
+        random_mask = np.random.permutation(range(total_length))
+        if dense:
+            return sparse_indices_sampled[random_mask], dense_indices_sampled[random_mask], \
+                   dense_values_sampled[random_mask], label_sampled[random_mask]
+        else:
+            return sparse_indices_sampled[random_mask], None, None, label_sampled[random_mask]
+
+    def sample_items_random(self, seed=42):
         np.random.seed(seed)
         item_indices_sampled = list()
         n_items = self.data_info.n_items
@@ -24,52 +39,64 @@ class SamplingBase(object):
         for u, i in zip(self.dataset.user_indices, self.dataset.item_indices):
             item_indices_sampled.append(i)
             for _ in range(self.num_neg):
-                item_neg = math.floor(random.random() * n_items)  # np.random.randint(n_items)
+                item_neg = math.floor(random.random() * n_items)
                 while item_neg in train_user_consumed[u]:
                     item_neg = math.floor(random.random() * n_items)
                 item_indices_sampled.append(item_neg)
         print("neg: ", time.time() - t0)
         return np.array(item_indices_sampled)
 
-    def sample_user_part(self):
-        pass
+    def sample_items_popular(self, seed=42):
+        np.random.seed(seed)
+        data = self.data_info.get_indexed_interaction()
+        item_counts = data.groupby("item")["user"].count()
+        train_user_consumed = self.dataset.train_user_consumed
+        items = np.arange(self.data_info.n_items)
 
-    def sample_item_part(self):
-        pass
+        item_order = list()
+        item_indices_sampled = list()
+        t1 = time.time()
+        for user, u_data in data.groupby("user", sort=False):
+            item_indices = u_data.index.to_list()
+            item_indices = item_indices * (self.num_neg + 1)
+            item_order.extend(item_indices)
+
+            item_indices_sampled.extend(u_data.item.tolist())  # add positive items
+            user_consumed = list(train_user_consumed[user])
+            u_item_counts = item_counts.to_numpy().copy()
+            u_item_counts[user_consumed] = 0
+            item_prob = u_item_counts / np.sum(u_item_counts)
+            neg_size = len(user_consumed) * self.num_neg
+            neg_sampled = np.random.choice(items, size=neg_size, p=item_prob, replace=True)
+            item_indices_sampled.extend(neg_sampled)
+        print("neg: ", time.time() - t1)
+
+        item_indices_sampled = np.asarray(item_indices_sampled)
+        item_order = np.argsort(item_order, kind="mergesort")  # must be stable sort to keep order
+        return item_indices_sampled[item_order]
 
 
 class NegativeSamplingFeat(SamplingBase):
     def __init__(self, dataset, data_info, num_neg, batch_size=64):
         super(NegativeSamplingFeat, self).__init__(dataset, data_info, num_neg, batch_size)
 
-    def __call__(self, seed=42, dense=True, shuffle=False, mode="random"):
+    def random_sampling(self, seed=42, dense=True, shuffle=False, mode="random"):  # "popular" may be slow
         if mode == "random":
-            return self.random_sampling(seed, dense, shuffle)
+            item_indices_sampled = self.sample_items_random(seed=seed)
         elif mode == "popular":
-            return
-        else:
-            raise ValueError("sampling mode must either be \"random\" or \"popular\"")
+            item_indices_sampled = self.sample_items_popular(seed=seed)
 
-    def random_sampling(self, seed=42, dense=True, shuffle=False):
-        item_indices_sampled = self.sample_items(seed=seed)
-        sparse_sampled = self._sparse_negative_sampling(item_indices_sampled)
-        dense_sampled = self._dense_negative_sampling(item_indices_sampled) if dense else None
+        sparse_indices_sampled = self._sparse_indices_sampling(item_indices_sampled)
+        dense_indices_sampled = self._dense_indices_sampling(item_indices_sampled) if dense else None
+        dense_values_sampled = self._dense_values_sampling(item_indices_sampled) if dense else None
         label_sampled = self._label_negative_sampling()
 
         if shuffle:
-            return self._shuffle_data(dense, sparse_sampled, dense_sampled, label_sampled, seed)
-        return sparse_sampled, dense_sampled, label_sampled
+            return self._shuffle_data(
+                dense, sparse_indices_sampled, dense_indices_sampled, dense_values_sampled, label_sampled, seed)
+        return sparse_indices_sampled, dense_indices_sampled, dense_values_sampled, label_sampled
 
-    def _shuffle_data(self, dense, sparse_sampled, dense_sampled, label_sampled, seed=42):
-        np.random.seed(seed)
-        total_length = len(self.dataset) * (self.num_neg + 1)
-        random_mask = np.random.permutation(range(total_length))
-        if dense:
-            return sparse_sampled[random_mask], dense_sampled[random_mask], label_sampled[random_mask]
-        else:
-            return sparse_sampled[random_mask], None, label_sampled[random_mask]
-
-    def _sparse_negative_sampling(self, item_indices_sampled):
+    def _sparse_indices_sampling(self, item_indices_sampled):
         user_sparse_col = self.data_info.user_sparse_col.index
         user_sparse_indices = np.take(self.dataset.sparse_indices, user_sparse_col, axis=1)
         user_sparse_sampled = np.repeat(user_sparse_indices, self.num_neg + 1, axis=0)
@@ -84,20 +111,34 @@ class NegativeSamplingFeat(SamplingBase):
         col_reindex = np.arange(len(orig_cols))[np.argsort(orig_cols)]
         return np.concatenate([user_sparse_sampled, item_sparse_sampled], axis=-1)[:, col_reindex]
 
-    def _dense_negative_sampling(self, item_indices_sampled):
+    def _dense_indices_sampling(self, item_indices_sampled):
+        n_samples = len(item_indices_sampled)
         user_dense_col = self.data_info.user_dense_col.index
-        user_dense_values = np.take(self.dataset.dense_values, user_dense_col, axis=1)
-        user_dense_sampled = np.repeat(user_dense_values, self.num_neg + 1, axis=0)
-
         item_dense_col = self.data_info.item_dense_col.index
-        item_dense_sampled = self.data_info.item_dense_unique[item_indices_sampled]
+        total_dense_cols = len(user_dense_col) + len(item_dense_col)
+        return np.tile(np.arange(total_dense_cols), [n_samples, 1])
 
-        assert len(user_dense_sampled) == len(item_dense_sampled), \
-            "num of user sampled must equal to num of item sampled"
-        # keep column names in original order
-        orig_cols = user_dense_col + item_dense_col
-        col_reindex = np.arange(len(orig_cols))[np.argsort(orig_cols)]
-        return np.concatenate([user_dense_sampled, item_dense_sampled], axis=-1)[:, col_reindex]
+    def _dense_values_sampling(self, item_indices_sampled):
+        user_dense_col = self.data_info.user_dense_col.index
+        item_dense_col = self.data_info.item_dense_col.index
+        if user_dense_col and item_dense_col:
+            user_dense_values = np.take(self.dataset.dense_values, user_dense_col, axis=1)
+            user_dense_sampled = np.repeat(user_dense_values, self.num_neg + 1, axis=0)
+            item_dense_sampled = self.data_info.item_dense_unique[item_indices_sampled]
+
+            assert len(user_dense_sampled) == len(item_dense_sampled), \
+                "num of user sampled must equal to num of item sampled"
+            # keep column names in original order
+            orig_cols = user_dense_col + item_dense_col
+            col_reindex = np.arange(len(orig_cols))[np.argsort(orig_cols)]
+            return np.concatenate([user_dense_sampled, item_dense_sampled], axis=-1)[:, col_reindex]
+        elif user_dense_col:
+            user_dense_values = np.take(self.dataset.dense_values, user_dense_col, axis=1)
+            user_dense_sampled = np.repeat(user_dense_values, self.num_neg + 1, axis=0)
+            return user_dense_sampled
+        elif item_dense_col:
+            item_dense_sampled = self.data_info.item_dense_unique[item_indices_sampled]
+            return item_dense_sampled
 
     def _label_negative_sampling(self):
         factor = self.num_neg + 1
