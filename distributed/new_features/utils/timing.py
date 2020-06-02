e8bf7106263607d39bd768aa2cb7e3e8a2cfe301
massquantity <wdmjjxg@163.com>
2020-06-02 20:41:13 +0800

    timing

---
+++ distributed/new_features/utils/timing.py
@@ -0,0 +1,25 @@
+import time
+import functools
+from contextlib import contextmanager
+
+
+def time_func(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{func.__name__} elapsed: {(end - start):3.3f}s")
+        return result
+    return wrapper
+
+
+@contextmanager
+def time_block(block_name="block"):
+    start = time.perf_counter()
+    try:
+        yield
+    finally:
+        end = time.perf_counter()
+        print(f"{block_name} elapsed: {(end - start):3.3f}s")
+
