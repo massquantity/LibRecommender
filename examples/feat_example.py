7716dc55ad34a620f010de8eaf4db745ec065268
massquantity <wdmjjxg@163.com>
2020-07-09 17:06:05 +0800

    examples

---
+++ examples/feat_example.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from libreco.data import split_by_ratio_chrono, DatasetFeat
+from libreco.algorithms import YouTubeRanking
+
+# remove unnecessary tensorflow logging
+import os
+import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["KMP_WARNINGS"] = "FALSE"
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+
+if __name__ == "__main__":
+    data = pd.read_csv("sample_data/sample_movielens_merged.csv",
+                       sep=",", header=0)
+    # convert to implicit data and do negative sampling afterwards
+    data["label"] = 1
+
+    # split into train and test data based on time
+    train_data, test_data = split_by_ratio_chrono(data, test_size=0.2)
+
+    # specify complete columns information
+    sparse_col = ["sex", "occupation", "genre1", "genre2", "genre3"]
+    dense_col = ["age"]
+    user_col = ["sex", "age", "occupation"]
+    item_col = ["genre1", "genre2", "genre3"]
+
+    train_data, data_info = DatasetFeat.build_trainset(
+        train_data, user_col, item_col, sparse_col, dense_col)
+    test_data = DatasetFeat.build_testset(test_data, sparse_col, dense_col)
+
+    # sample negative items for each record
+    train_data.build_negative_samples(data_info)
+    test_data.build_negative_samples(data_info)
+    print(data_info)  # n_users: 5962, n_items: 3226, data sparsity: 0.4185 %
+
+    ytb_ranking = YouTubeRanking(task="ranking", data_info=data_info,
+                                 embed_size=16, n_epochs=3, lr=1e-4,
+                                 batch_size=512, use_bn=True,
+                                 hidden_units="128,64,32")
+    ytb_ranking.fit(train_data, verbose=2, shuffle=True,
+                    eval_data=test_data,
+                    metrics=["loss", "roc_auc", "precision",
+                             "recall", "map", "ndcg"])
+
+    # predict preference of user 1 to item 2333
+    print("prediction: ", ytb_ranking.predict(user=1, item=2333))
+    # recommend 7 items for user 1
+    print("recommendation: ", ytb_ranking.recommend_user(user=1, n_rec=7))
+
+
---
+++ examples/pure_example.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from libreco.data import random_split, DatasetPure
+from libreco.algorithms import SVDpp  # pure data, algorithm SVD++
+
+# remove unnecessary tensorflow logging
+import os
+import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["KMP_WARNINGS"] = "FALSE"
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+
+if __name__ == "__main__":
+    data = pd.read_csv("sample_data/sample_movielens_rating.dat", sep="::",
+                       names=["user", "item", "label", "time"])
+
+    # split whole data into three folds for training, evaluating and testing
+    train_data, eval_data, test_data = random_split(
+        data, multi_ratios=[0.8, 0.1, 0.1])
+
+    train_data, data_info = DatasetPure.build_trainset(train_data)
+    eval_data = DatasetPure.build_testset(eval_data)
+    test_data = DatasetPure.build_testset(test_data)
+    print(data_info)   # n_users: 5894, n_items: 3253, data sparsity: 0.4172 %
+
+    svdpp = SVDpp(task="rating", data_info=data_info, embed_size=16,
+                  n_epochs=3, lr=0.001, reg=None, batch_size=256)
+    # monitor metrics on eval_data during training
+    svdpp.fit(train_data, verbose=2, eval_data=eval_data,
+              metrics=["rmse", "mae", "r2"])
+
+    # do final evaluation on test data
+    svdpp.evaluate(test_data, metrics=["rmse", "mae"])
+    # predict preference of user 1 to item 2333
+    print("prediction: ", svdpp.predict(user=1, item=2333))
+    # recommend 7 items for user 1
+    print("recommendation: ", svdpp.recommend_user(user=1, n_rec=7))
+
+
