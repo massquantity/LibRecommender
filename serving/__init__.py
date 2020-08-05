4126a7a3fbc3548a61cf7d990357930d562920c4
massquantity <wdmjjxg@163.com>
2020-08-05 12:33:34 +0800

    change serving

--- serving/__init__.py
+++ serving/__init__.py
@@ -1,9 +1,1 @@
-from .flask.save_redis import (
-    sim2redis,
-    vector2redis,
-    data_info2redis,
-    user_consumed2redis,
-    seq2redis
-)
-from .flask.colorize import colorize
-from .flask.save_faiss import save_faiss_index
+from . import flask
