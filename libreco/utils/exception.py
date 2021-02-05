f6bd443e73c7b50f46f1c40347a678416e96208a
massquantity <wdmjjxg@163.com>
2021-02-05 16:31:52 +0800

    utils

--- libreco/utils/exception.py
+++ libreco/utils/exception.py
@@ -1,7 +1,7 @@
 class NotSamplingError(Exception):
     """Exception related to sampling data
 
-    If client wants to use batch_sampling and then evaluate on the dataset,
+    If client wants to use batch_sampling and then evaluation on the dataset,
     but forgot to do whole data sampling beforehand, this exception will be
     raised. Because in this case, unsampled data can't be evaluated.
     """
