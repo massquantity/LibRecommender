c4fd80a061939ef41e0bf325ed99a63ceadbfb57
massquantity <wdmjjxg@163.com>
2022-09-19 01:07:04 +0800

    remove flask

--- serving/flask/__init__.py
+++
@@ -1,9 +0,0 @@
-from .save_redis import (
-    sim2redis,
-    vector2redis,
-    data_info2redis,
-    user_consumed2redis,
-    seq2redis
-)
-from .save_faiss import save_faiss_index
-from .colorize import colorize
