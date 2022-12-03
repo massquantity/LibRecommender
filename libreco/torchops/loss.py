aaecb39146d84362d0b139a3b8e7272ecc34f6e2
massquantity <wdmjjxg@163.com>
2022-12-03 19:59:40 +0800

    add torch losses

---
+++ libreco/torchops/loss.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn.functional as F
+
+
+def binary_cross_entropy_loss(logits, labels):
+    return F.binary_cross_entropy_with_logits(logits, labels)
+
+
+# focal loss for binary cross entropy based on [Lin et al., 2018](https://arxiv.org/pdf/1708.02002.pdf)
+def focal_loss(logits, labels, alpha=0.25, gamma=2.0):
+    weighting_factor = (labels * alpha) + ((1 - labels) * (1 - alpha))
+    probs = torch.sigmoid(logits)
+    p_t = (labels * probs) + ((1 - labels) * (1 - probs))
+    modulating_factor = torch.pow(1.0 - p_t, gamma)
+    bce = F.binary_cross_entropy_with_logits(logits, labels, reduction="none")
+    return torch.mean(weighting_factor * modulating_factor * bce)
+
+
+def bpr_loss(targets, items_pos, items_neg):
+    pos_scores, neg_scores = compute_pair_scores(targets, items_pos, items_neg)
+    log_sigmoid = F.logsigmoid(pos_scores - neg_scores)
+    return torch.negative(torch.mean(log_sigmoid))
+
+
+def max_margin_loss(targets, items_pos, items_neg, margin):
+    pos_scores, neg_scores = compute_pair_scores(targets, items_pos, items_neg)
+    return F.margin_ranking_loss(
+        pos_scores, neg_scores, torch.ones_like(pos_scores), margin=margin
+    )
+
+
+def graphsage_unsupervised_loss(targets, items_pos, items_neg, neg_weights=1.0):
+    pos_scores, neg_scores = compute_pair_scores(
+        targets, items_pos, items_neg, repeat_positives=False
+    )
+    pos_bce = F.binary_cross_entropy_with_logits(
+        pos_scores, torch.ones_like(pos_scores)
+    )
+    neg_bce = F.binary_cross_entropy_with_logits(
+        neg_scores, torch.zeros_like(neg_scores)
+    )
+    return pos_bce.sum() + neg_weights * neg_bce.sum()
+
+
+def compute_pair_scores(targets, items_pos, items_neg, repeat_positives=True):
+    if len(targets) == len(items_pos) == len(items_neg):
+        pos_scores = torch.sum(torch.mul(targets, items_pos), dim=1)
+        neg_scores = torch.sum(torch.mul(targets, items_neg), dim=1)
+        return pos_scores, neg_scores
+
+    if len(targets) != len(items_pos):
+        raise ValueError(
+            f"targets and items_pos length doesn't match, "
+            f"got {len(targets)} and {len(items_pos)}"
+        )
+    pos_len, neg_len = len(items_pos), len(items_neg)
+    if neg_len % pos_len != 0:
+        raise ValueError(
+            f"negatives length is not a multiple of positives length, "
+            f"got {neg_len} and {pos_len}"
+        )
+    factor = int(len(items_neg) / len(items_pos))
+    embed_size = items_pos.shape[1]
+    targets_neg = targets.unsqueeze(1)
+    items_neg = items_neg.view(pos_len, factor, embed_size)
+    pos_scores = torch.sum(torch.mul(targets, items_pos), dim=1)
+    if repeat_positives:
+        pos_scores = pos_scores.repeat_interleave(factor)
+    neg_scores = torch.mul(targets_neg, items_neg).view(-1, embed_size)
+    neg_scores = torch.sum(neg_scores, dim=1)
+    return pos_scores, neg_scores
