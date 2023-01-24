1286dce5ff9076d37fc3e8d1c038f7e2e43555a0
massquantity <wdmjjxg@163.com>
2023-01-24 19:47:05 +0800

    [Test] add dense features in test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import pytest
 import pandas as pd
 
@@ -30,9 +31,9 @@ def prepare_feat_data(read_feat_data):
     train_data, data_info = DatasetFeat.build_trainset(
         train_data=train_data,
         sparse_col=["sex", "occupation", "genre1", "genre2", "genre3"],
-        dense_col=["age"],
+        dense_col=["age", "item_dense_feat"],
         user_col=["sex", "age", "occupation"],
-        item_col=["genre1", "genre2", "genre3"],
+        item_col=["genre1", "genre2", "genre3", "item_dense_feat"],
     )
     eval_data = DatasetFeat.build_testset(eval_data)
     yield pd_data, train_data, eval_data, data_info
@@ -46,9 +47,9 @@ def prepare_multi_sparse_data(read_feat_data):
         train_data=train_data,
         sparse_col=["sex", "occupation"],
         multi_sparse_col=[["genre1", "genre2", "genre3"]],
-        dense_col=["age"],
+        dense_col=["age", "item_dense_feat"],
         user_col=["sex", "age", "occupation"],
-        item_col=["genre1", "genre2", "genre3"],
+        item_col=["genre1", "genre2", "genre3", "item_dense_feat"],
         pad_val=["missing"],
     )
     eval_data = DatasetFeat.build_testset(eval_data)
@@ -64,4 +65,5 @@ def read_feat_data():
         "sample_movielens_merged.csv",
     )
     pd_data = pd.read_csv(data_path, sep=",", header=0)
+    pd_data["item_dense_feat"] = np.random.randn(len(pd_data), 1)
     return pd_data, split_by_ratio_chrono(pd_data, test_size=0.2)
