ca954b7c14f3cff76dd319f7acb50d973efb44c8
massquantity <wdmjjxg@163.com>
2023-01-26 22:35:59 +0800

    [Lint] ignore long line in flake8

--- libreco/algorithms/wide_deep.py
+++ libreco/algorithms/wide_deep.py
@@ -1,6 +1,7 @@
 """
 
-Reference: Heng-Tze Cheng et al. "Wide & Deep Learning for Recommender Systems"  (https://arxiv.org/pdf/1606.07792.pdf)
+Reference: Heng-Tze Cheng et al. "Wide & Deep Learning for Recommender Systems"
+           (https://arxiv.org/pdf/1606.07792.pdf)
 
 author: massquantity
 
