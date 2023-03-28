69d808f6198b6796b8f3a67a0edfaba2cd2f0d64
massquantity <wdmjjxg@163.com>
2023-03-28 14:19:58 +0800

    [Struct] Refactor evaluation (#253)

--- libreco/evaluation/evaluate.py
+++ libreco/evaluation/evaluate.py
@@ -1,19 +1,11 @@
 """Utility Functions for Evaluating Data."""
+import functools
 import math
 import numbers
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import (
-    auc,
-    balanced_accuracy_score,
-    log_loss,
-    mean_absolute_error,
-    mean_squared_error,
-    precision_recall_curve,
-    r2_score,
-    roc_auc_score,
-)
+from sklearn.metrics import log_loss, mean_absolute_error, r2_score, roc_auc_score
 
 from .computation import (
     build_eval_transformed_data,
@@ -22,13 +14,18 @@ from .computation import (
     compute_recommends,
 )
 from .metrics import (
-    ALLOWED_METRICS,
     LISTWISE_METRICS,
     POINTWISE_METRICS,
-    map_at_k,
+    RANKING_METRICS,
+    RATING_METRICS,
+    average_precision_at_k,
+    balanced_accuracy,
+    listwise_scores,
     ndcg_at_k,
+    pr_auc_score,
     precision_at_k,
     recall_at_k,
+    rmse,
 )
 from ..data import TransformedSet
 
@@ -38,143 +35,36 @@ def _check_metrics(task, metrics, k):
         metrics = [metrics]
     if task == "rating":
         for m in metrics:
-            if m not in ALLOWED_METRICS["rating_metrics"]:
-                raise ValueError(f"metrics {m} is not suitable for rating task...")
+            if m not in RATING_METRICS:
+                raise ValueError(f"Metrics `{m}` is not suitable for rating task...")
     elif task == "ranking":
         for m in metrics:
-            if m not in ALLOWED_METRICS["ranking_metrics"]:
-                raise ValueError(f"metrics {m} is not suitable for ranking task...")
+            if m not in RANKING_METRICS:
+                raise ValueError(f"Metrics `{m}` is not suitable for ranking task...")
 
     if not isinstance(k, numbers.Integral):
-        raise TypeError("k must be integer")
-
+        raise TypeError("`k` must be integer")
     return metrics
 
 
-def print_metrics(
-    model,
-    train_data=None,
-    eval_data=None,
-    metrics=None,
-    eval_batch_size=8192,
-    k=10,
-    sample_user_num=2048,
-    seed=42,
-):
-    if not metrics:
-        metrics = ["loss"]
-    metrics = _check_metrics(model.task, metrics, k)
-
-    if model.task == "rating":
-        if train_data:
-            y_pred, y_true = compute_preds(model, train_data, eval_batch_size)
-            print_metrics_rating(metrics, y_true, y_pred, train=True)
-        if eval_data:
-            y_pred, y_true = compute_preds(model, eval_data, eval_batch_size)
-            print_metrics_rating(metrics, y_true, y_pred, train=False)
-
-    elif model.task == "ranking":
-        if train_data and POINTWISE_METRICS.intersection(metrics):
-            y_prob, y_true = compute_probs(model, train_data, eval_batch_size)
-            log_loss_ = log_loss(y_true, y_prob, eps=1e-7)
-            print(f"\t train log_loss: {log_loss_:.4f}")
-        if eval_data:
-            if POINTWISE_METRICS.intersection(metrics):
-                y_prob, y_true = compute_probs(model, eval_data, eval_batch_size)
-                print_metrics_ranking_pointwise(metrics, y_prob, y_true)
-            if LISTWISE_METRICS.intersection(metrics):
-                chosen_users = sample_user(eval_data, seed, sample_user_num)
-                num_batch_users = max(1, math.floor(eval_batch_size / model.n_items))
-                y_true_list = eval_data.user_consumed
-                y_reco_list, users = compute_recommends(
-                    model, chosen_users, k, num_batch_users
-                )
-                print_metrics_ranking_listwise(
-                    metrics, y_reco_list, y_true_list, users, k
-                )
-
-
-# noinspection PyTypeChecker
-def sample_user(data, seed, num):
-    np.random.seed(seed)
-    unique_users = np.unique(data.user_indices)
+def sample_users(data, seed, num):
+    np_rng = np.random.default_rng(seed)
+    unique_users = list(data.user_consumed)
     if isinstance(num, numbers.Integral) and 0 < num < len(unique_users):
-        users = np.random.choice(unique_users, num, replace=False)
+        users = np_rng.choice(unique_users, num, replace=False).tolist()
     else:
         users = unique_users
-    if isinstance(users, np.ndarray):
-        users = list(users)
     return users
-
-
-def print_metrics_rating(metrics, y_true, y_pred, train=True, **kwargs):
-    if kwargs.get("lower_bound") and kwargs.get("upper_bound"):
-        lower_bound, upper_bound = (
-            kwargs.get("lower_bound"),
-            kwargs.get("upper_bound"),
-        )
-        y_pred = np.clip(y_pred, lower_bound, upper_bound)
-    if train:
-        for m in metrics:
-            if m in ["rmse", "loss"]:
-                rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-                print(f"\t train rmse: {rmse:.4f}")
-    else:
-        for m in metrics:
-            if m in ["rmse", "loss"]:
-                rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-                print(f"\t eval rmse: {rmse:.4f}")
-            elif m == "mae":
-                mae = mean_absolute_error(y_true, y_pred)
-                print(f"\t eval mae: {mae:.4f}")
-            elif m == "r2":
-                r_squared = r2_score(y_true, y_pred)
-                print(f"\t eval r2: {r_squared:.4f}")
-
-
-def print_metrics_ranking_pointwise(metrics, y_prob, y_true):
-    for m in metrics:
-        if m in ["log_loss", "loss"]:
-            log_loss_ = log_loss(y_true, y_prob, eps=1e-7)
-            print(f"\t eval log_loss: {log_loss_:.4f}")
-        elif m == "balanced_accuracy":
-            y_pred = np.round(y_prob)
-            accuracy = balanced_accuracy_score(y_true, y_pred)
-            print(f"\t eval balanced accuracy: {accuracy:.4f}")
-        elif m == "roc_auc":
-            roc_auc = roc_auc_score(y_true, y_prob)
-            print(f"\t eval roc_auc: {roc_auc:.4f}")
-        elif m == "pr_auc":
-            precision, recall, _ = precision_recall_curve(y_true, y_prob)
-            pr_auc = auc(recall, precision)
-            print(f"\t eval pr_auc: {pr_auc:.4f}")
-
-
-def print_metrics_ranking_listwise(metrics, y_reco_list, y_true_list, users, k):
-    for m in metrics:
-        if m == "precision":
-            precision_all = precision_at_k(y_true_list, y_reco_list, users, k)
-            print(f"\t eval precision@{k}: {precision_all:.4f}")
-        elif m == "recall":
-            recall_all = recall_at_k(y_true_list, y_reco_list, users, k)
-            print(f"\t eval recall@{k}: {recall_all:.4f}")
-        elif m == "map":
-            map_all = map_at_k(y_true_list, y_reco_list, users, k)
-            print(f"\t eval map@{k}: {map_all:.4f}")
-        elif m == "ndcg":
-            ndcg_all = ndcg_at_k(y_true_list, y_reco_list, users, k)
-            print(f"\t eval ndcg@{k}: {ndcg_all:.4f}")
 
 
 def evaluate(
     model,
     data,
+    neg_sampling,
     eval_batch_size=8192,
     metrics=None,
     k=10,
-    sample_user_num=2048,
-    neg_sample=False,
-    update_features=False,
+    sample_user_num=None,
     seed=42,
 ):
     """Evaluate the model on specific data and metrics.
@@ -183,84 +73,110 @@ def evaluate(
     ----------
     model : Base
         Model for evaluation.
-    data : pandas.DataFrame or :class:`~libreco.data.TransformedSet`
+    data : :class:`pandas.DataFrame` or :class:`~libreco.data.TransformedSet`
         Data to evaluate.
+    neg_sampling : bool
+        Whether to perform negative sampling for evaluating data.
     eval_batch_size : int, default: 8192
         Batch size used in evaluation.
     metrics : list or None, default: None
         List of metrics for evaluating.
     k : int, default: 10
         Parameter of metrics, e.g. recall at k, ndcg at k
-    sample_user_num : int, default: 2048
-        Number of users for evaluating. Setting it to a positive number will sample
-        users randomly from eval data.
-    neg_sample : bool, default: False
-        Whether to do negative sampling when evaluating.
-    update_features : bool, default: False
-        Whether to update model's ``data_info`` from features in data.
+    sample_user_num : int or None, default: None
+        Number of users used in evaluating. By default, it will use all the users in eval_data.
+        Setting it to a positive number will sample users randomly from eval data.
     seed : int, default: 42
         Random seed.
 
     Returns
     -------
-    results : dict of {str : float}
+    eval_results : dict of {str : float}
         Evaluation results for the model and data.
 
     Examples
     --------
-    >>> eval_result = evaluate(model, data, metrics=["roc_auc", "precision", "recall"])
+    >>> eval_result = evaluate(model, data, neg_sampling=True, metrics=["roc_auc", "precision", "recall"])
     """
-    if isinstance(data, pd.DataFrame):
-        data = build_eval_transformed_data(
-            model, data, neg_sample, update_features, seed
-        )
-    assert isinstance(
-        data, TransformedSet
-    ), "The data from evaluation must be TransformedSet object."
+    if not isinstance(data, (pd.DataFrame, TransformedSet)):
+        raise ValueError("`data` must be `pandas.DataFrame` or `TransformedSet`")
+    data = build_eval_transformed_data(model, data, neg_sampling, seed)
     if not metrics:
         metrics = ["loss"]
     metrics = _check_metrics(model.task, metrics, k)
     eval_result = dict()
-
     if model.task == "rating":
         y_pred, y_true = compute_preds(model, data, eval_batch_size)
         for m in metrics:
             if m in ["rmse", "loss"]:
-                eval_result[m] = np.sqrt(mean_squared_error(y_true, y_pred))
+                eval_result[m] = rmse(y_true, y_pred)
             elif m == "mae":
                 eval_result[m] = mean_absolute_error(y_true, y_pred)
             elif m == "r2":
                 eval_result[m] = r2_score(y_true, y_pred)
-
-    elif model.task == "ranking":
+    else:
         if POINTWISE_METRICS.intersection(metrics):
             y_prob, y_true = compute_probs(model, data, eval_batch_size)
             for m in metrics:
                 if m in ["log_loss", "loss"]:
                     eval_result[m] = log_loss(y_true, y_prob, eps=1e-7)
                 elif m == "balanced_accuracy":
-                    y_pred = np.round(y_prob)
-                    eval_result[m] = balanced_accuracy_score(y_true, y_pred)
+                    eval_result[m] = balanced_accuracy(y_true, y_prob)
                 elif m == "roc_auc":
                     eval_result[m] = roc_auc_score(y_true, y_prob)
                 elif m == "pr_auc":
-                    precision, recall, _ = precision_recall_curve(y_true, y_prob)
-                    eval_result[m] = auc(recall, precision)
+                    eval_result[m] = pr_auc_score(y_true, y_prob)
         if LISTWISE_METRICS.intersection(metrics):
-            chosen_users = sample_user(data, seed, sample_user_num)
+            users = sample_users(data, seed, sample_user_num)
             num_batch_users = max(1, math.floor(eval_batch_size / model.n_items))
-            y_true_list = data.user_consumed
-            y_reco_list, users = compute_recommends(
-                model, chosen_users, k, num_batch_users
-            )
+            y_trues = data.user_consumed
+            y_recos = compute_recommends(model, users, k, num_batch_users)
             for m in metrics:
+                if m not in LISTWISE_METRICS:
+                    continue
                 if m == "precision":
-                    eval_result[m] = precision_at_k(y_true_list, y_reco_list, users, k)
+                    fn = precision_at_k
                 elif m == "recall":
-                    eval_result[m] = recall_at_k(y_true_list, y_reco_list, users, k)
+                    fn = recall_at_k
                 elif m == "map":
-                    eval_result[m] = map_at_k(y_true_list, y_reco_list, users, k)
+                    fn = average_precision_at_k
                 elif m == "ndcg":
-                    eval_result[m] = ndcg_at_k(y_true_list, y_reco_list, users, k)
+                    fn = ndcg_at_k
+                # noinspection PyUnboundLocalVariable
+                eval_result[m] = listwise_scores(fn, y_trues, y_recos, users, k)
 
     return eval_result
+
+
+def print_metrics(
+    model,
+    neg_sampling,
+    train_data=None,
+    eval_data=None,
+    metrics=None,
+    eval_batch_size=8192,
+    k=10,
+    sample_user_num=2048,
+    seed=42,
+):
+    loss_name = "rmse" if model.task == "rating" else "log_loss"
+    metrics_fn = functools.partial(
+        evaluate,
+        model=model,
+        neg_sampling=neg_sampling,
+        eval_batch_size=eval_batch_size,
+        k=k,
+        sample_user_num=sample_user_num,
+        seed=seed,
+    )
+    if train_data:
+        train_metrics = metrics_fn(data=train_data, metrics=[loss_name])
+        print(f"\t train {loss_name}: {train_metrics[loss_name]:.4f}")
+    if eval_data:
+        eval_metrics = metrics_fn(data=eval_data, metrics=metrics)
+        for m, val in eval_metrics.items():
+            if m == "loss":
+                m = loss_name
+            if m in LISTWISE_METRICS:
+                m = f"{m}@{k}"
+            print(f"\t eval {m}: {val:.4f}")
--- libreco/evaluation/metrics.py
+++ libreco/evaluation/metrics.py
@@ -1,87 +1,73 @@
 import numpy as np
+from sklearn.metrics import (
+    auc,
+    balanced_accuracy_score,
+    mean_squared_error,
+    precision_recall_curve,
+)
 
-# pairwise_metrics = bpr ??
+RATING_METRICS = {"loss", "rmse", "mae", "r2"}
 POINTWISE_METRICS = {"loss", "log_loss", "balanced_accuracy", "roc_auc", "pr_auc"}
 LISTWISE_METRICS = {"precision", "recall", "map", "ndcg"}
-ALLOWED_METRICS = {
-    "rating_metrics": ["loss", "rmse", "mae", "r2"],
-    "ranking_metrics": [
-        "loss",
-        "log_loss",
-        "balanced_accuracy",
-        "roc_auc",
-        "pr_auc",
-        "precision",
-        "recall",
-        "map",
-        "ndcg",
-    ],
-}
+RANKING_METRICS = POINTWISE_METRICS | LISTWISE_METRICS
 
 
-def precision_at_k(y_true_list, y_reco_list, users, k):
-    precision_all = list()
+def rmse(y_true, y_pred):
+    return np.sqrt(mean_squared_error(y_true, y_pred))
+
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.balanced_accuracy_score.html
+def balanced_accuracy(y_true, y_prob):
+    y_pred = np.round(y_prob)
+    return balanced_accuracy_score(y_true, y_pred)
+
+
+def pr_auc_score(y_true, y_prob):
+    precision, recall, _ = precision_recall_curve(y_true, y_prob)
+    return auc(recall, precision)
+
+
+def listwise_scores(fn, y_true_lists, y_reco_lists, users, k):
+    scores = list()
     for u in users:
-        y_true = y_true_list[u]
-        y_reco = y_reco_list[u]
-        common_items = set(y_reco).intersection(y_true)
-        precision = len(common_items) / k
-        precision_all.append(precision)
-    return np.mean(precision_all)
+        y_true = y_true_lists[u]
+        y_reco = y_reco_lists[u]
+        scores.append(fn(y_true, y_reco, k))
+    return np.mean(scores)
 
 
-def recall_at_k(y_true_list, y_reco_list, users, k):
-    recall_all = list()
-    for u in users:
-        y_true = y_true_list[u]
-        y_reco = y_reco_list[u]
-        common_items = set(y_reco).intersection(y_true)
-        recall = len(common_items) / len(y_true)
-        recall_all.append(recall)
-    return np.mean(recall_all)
+def precision_at_k(y_true, y_reco, k):
+    common_items = set(y_reco).intersection(y_true)
+    return len(common_items) / k
+
+
+def recall_at_k(y_true, y_reco, _k):
+    common_items = set(y_reco).intersection(y_true)
+    return len(common_items) / len(y_true)
 
 
 def average_precision_at_k(y_true, y_reco, k):
     common_items, _, indices_in_reco = np.intersect1d(
         y_true, y_reco, assume_unique=True, return_indices=True
     )
-
-    rank_list = np.zeros(k)
-    if common_items.size > 0:
-        rank_list[indices_in_reco] = 1
-        ap = [np.mean(rank_list[: i + 1]) for i in range(k) if rank_list[i]]
-        assert len(ap) == common_items.size, "common size doesn't match..."
-        return np.mean(ap)
-    else:
+    if common_items.size == 0:
         return 0
+    rank_list = np.zeros(k, np.float32)
+    rank_list[indices_in_reco] = 1
+    ap = [np.mean(rank_list[: i + 1]) for i in range(k) if rank_list[i]]
+    assert len(ap) == common_items.size, "common size doesn't match..."
+    return np.mean(ap)
 
 
-def map_at_k(y_true_list, y_reco_list, users, k):
-    map_all = list()
-    for u in users:
-        y_true = list(set(y_true_list[u]))
-        y_reco = y_reco_list[u]
-        map_all.append(average_precision_at_k(y_true, y_reco, k))
-    return np.mean(map_all)
-
-
-def ndcg_at_k(y_true_list, y_reco_list, users, k):
-    ndcg_all = list()
-    for u in users:
-        rank_list = np.zeros(k)
-        y_true = list(set(y_true_list[u]))
-        y_reco = y_reco_list[u]
-        common_items, _, indices_in_reco = np.intersect1d(
-            y_true, y_reco, assume_unique=True, return_indices=True
-        )
-
-        if common_items.size > 0:
-            rank_list[indices_in_reco] = 1
-            ideal_list = np.sort(rank_list)[::-1]
-            dcg = np.sum(rank_list / np.log2(np.arange(2, k + 2)))
-            idcg = np.sum(ideal_list / np.log2(np.arange(2, k + 2)))
-            ndcg = dcg / idcg
-        else:
-            ndcg = 0
-        ndcg_all.append(ndcg)
-    return np.mean(ndcg_all)
+def ndcg_at_k(y_true, y_reco, k):
+    common_items, _, indices_in_reco = np.intersect1d(
+        y_true, y_reco, assume_unique=True, return_indices=True
+    )
+    if common_items.size == 0:
+        return 0
+    rank_list = np.zeros(k, np.float32)
+    rank_list[indices_in_reco] = 1
+    ideal_list = np.sort(rank_list)[::-1]
+    dcg = np.sum(rank_list / np.log2(np.arange(2, k + 2)))
+    idcg = np.sum(ideal_list / np.log2(np.arange(2, k + 2)))
+    return dcg / idcg
