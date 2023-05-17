151a11e4d379102fd654149623df7932e681b8b2
massquantity <wdmjjxg@163.com>
2023-05-17 16:35:00 +0800

    [Test] Test TwoTower model

--- tests/models/test_two_tower.py
+++ tests/models/test_two_tower.py
@@ -12,27 +12,39 @@ from tests.utils_save_load import save_load_model
 
 
 @pytest.mark.parametrize(
-    "task, neg_sampling",
+    "task, loss_type, use_correction, temperature, sampler, neg_sampling",
     [
-        ("rating", None),
-        ("ranking", False),
-        ("ranking", True),
+        ("rating", "softmax", True, 1.0, "random", True),
+        ("ranking", "whatever", True, 1.0, "random", True),
+        ("ranking", "softmax", True, 1.0, "random", None),
+        ("ranking", "cross_entropy", True, 1.0, "random", False),
+        ("ranking", "cross_entropy", True, 1.0, "random", True),
+        ("ranking", "max_margin", True, 1.0, "unconsumed", True),
+        ("ranking", "max_margin", True, 1.0, "unconsumed", False),
+        ("ranking", "softmax", True, 2.0, "popular", True),
+        ("ranking", "softmax", False, 0.0, "unconsumed", True),
+        ("ranking", "softmax", True, -1, "random", False),
     ],
 )
 @pytest.mark.parametrize(
-    "norm_embed, lr_decay, reg, dropout_rate, hidden_units, num_workers",
+    "norm_embed, lr_decay, reg, use_bn, dropout_rate, hidden_units, num_workers",
     [
-        (True, False, None, None, 1, 0),
-        (False, True, 0.001, 0.5, (32, 16), 2),
+        (True, False, None, True, None, 1, 0),
+        (False, True, 0.001, False, 0.5, (32, 16), 2),
     ],
 )
 def test_two_tower(
     feat_data_small,
     task,
+    loss_type,
+    use_correction,
+    temperature,
+    sampler,
     neg_sampling,
     norm_embed,
     lr_decay,
     reg,
+    use_bn,
     dropout_rate,
     hidden_units,
     num_workers,
@@ -47,13 +59,20 @@ def test_two_tower(
         set_ranking_labels(train_data)
         set_ranking_labels(eval_data)
 
-    if task == "rating":
+    if task == "rating" or loss_type == "whatever":
+        with pytest.raises(ValueError):
+            _ = TwoTower(task, data_info, loss_type)
+    elif neg_sampling is None:
         with pytest.raises(AssertionError):
-            _ = TwoTower(task, data_info)
+            TwoTower(task, data_info).fit(train_data, neg_sampling)
+    elif loss_type == "max_margin" and not neg_sampling:
+        with pytest.raises(ValueError):
+            TwoTower(task, data_info, loss_type).fit(train_data, neg_sampling)
     else:
         model = TwoTower(
             task=task,
             data_info=data_info,
+            loss_type=loss_type,
             embed_size=4,
             norm_embed=norm_embed,
             n_epochs=1,
@@ -61,8 +80,12 @@ def test_two_tower(
             lr_decay=lr_decay,
             reg=reg,
             batch_size=100,
+            sampler=sampler,
+            use_bn=use_bn,
             dropout_rate=dropout_rate,
             hidden_units=hidden_units,
+            use_correction=use_correction,
+            temperature=temperature,
             tf_sess_config=None,
         )
         model.fit(
