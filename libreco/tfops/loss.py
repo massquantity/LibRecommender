cc9b226daaaad9e67f7632dcf1b54cd1fe65e138
massquantity <wdmjjxg@163.com>
2023-05-17 16:31:19 +0800

    [Loss] Add max-margin loss

--- libreco/tfops/loss.py
+++ libreco/tfops/loss.py
@@ -23,9 +23,18 @@ def choose_tf_loss(model, task, loss_type):
             loss = -tf.reduce_mean(model.bpr_loss)
         elif loss_type == "focal":
             loss = tf.reduce_mean(focal_loss(labels=model.labels, logits=model.output))
+        elif loss_type == "max_margin":
+            loss = tf.reduce_mean(
+                max_margin_loss(
+                    model.user_vector,
+                    model.item_vector,
+                    model.item_vector_neg,
+                    model.margin,
+                )
+            )
         elif loss_type == "softmax":
             loss = tf.reduce_mean(
-                softmax_cross_entropy(model.user_vector, model.item_vector)
+                softmax_cross_entropy(model, model.user_vector, model.item_vector)
             )
         else:
             raise ValueError(f"unknown loss_type for ranking: {loss_type}")
@@ -43,7 +52,14 @@ def focal_loss(labels, logits, alpha=0.25, gamma=2.0):
     return weighting_factor * modulating_factor * bce
 
 
-def softmax_cross_entropy(user_embeds, item_embeds):
+def max_margin_loss(user_embeds, item_embeds, item_embeds_neg, margin):
+    pos_scores = tf.reduce_sum(user_embeds * item_embeds, axis=1)
+    neg_scores = tf.reduce_sum(user_embeds * item_embeds_neg, axis=1)
+    return tf.nn.relu(margin + neg_scores - pos_scores)
+
+
+def softmax_cross_entropy(model, user_embeds, item_embeds):
     logits = tf.matmul(user_embeds, item_embeds, transpose_b=True)
+    logits = model.adjust_logits(logits)
     labels = tf.range(tf.shape(user_embeds)[0])
     return tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
