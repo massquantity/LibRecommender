07d403e0c6a8d742169fe51fd598bb771fc8e9a1
massquantity <wdmjjxg@163.com>
2023-07-12 00:20:29 +0800

    [Struct] New embedding layer (#339)

--- libreco/layers/__init__.py
+++ libreco/layers/__init__.py
@@ -1,12 +1,16 @@
 from .convolutional import conv_nn, max_pool
 from .dense import dense_nn, shared_dense, tf_dense
+from .embedding import embedding_lookup, seq_embeds_pooling, sparse_embeds_pooling
 from .recurrent import tf_rnn
 
 __all__ = [
     "conv_nn",
     "dense_nn",
+    "embedding_lookup",
     "max_pool",
     "shared_dense",
+    "seq_embeds_pooling",
+    "sparse_embeds_pooling",
     "tf_dense",
     "tf_rnn",
 ]
