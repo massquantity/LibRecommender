[Fix] Remove `num_heads` in tf attention mask (#388)

libreco/algorithms/transformer.py
@@ -319,13 +319,9 @@ class Transformer(TfBase, metaclass=ModelMeta):
             return att_out + ffn_out
 
     def _transformer_mask(self, batch_size):
-        tfm_mask = compute_seq_mask(
-            self.user_interacted_len, self.max_seq_len, self.num_heads
-        )
+        tfm_mask = compute_seq_mask(self.user_interacted_len, self.max_seq_len)
         if self.use_causal_mask:
-            causal_mask = compute_causal_mask(
-                batch_size, self.max_seq_len, self.num_heads
-            )
+            causal_mask = compute_causal_mask(batch_size, self.max_seq_len)
             tfm_mask = tf.logical_and(tfm_mask, causal_mask)
         return tfm_mask
 

libreco/layers/transformer.py
@@ -34,7 +34,7 @@ def transformer_encoder_layer(
     Output shape: (batch_size, seq_len, embed_size)
     """
     with tf.variable_scope("transformer_encoder"):
-        att_mask = compute_seq_mask(seq_lens, max_seq_len, num_heads)
+        att_mask = compute_seq_mask(seq_lens, max_seq_len)
 
         pe = positional_encoding(max_seq_len, embed_size)[tf.newaxis, :, :]
         # scale the inputs before adding pe
@@ -82,8 +82,8 @@ def transformer_decoder_layer(
     Output shape: (batch_size, seq_len, embed_size)
     """
     with tf.variable_scope("transformer_decoder"):
-        seq_mask = compute_seq_mask(seq_lens, max_seq_len, num_heads)
-        causal_mask = compute_causal_mask(tf.shape(seqs)[0], max_seq_len, num_heads)
+        seq_mask = compute_seq_mask(seq_lens, max_seq_len)
+        causal_mask = compute_causal_mask(tf.shape(seqs)[0], max_seq_len)
         att_mask = seq_mask & causal_mask
 
         pe = positional_encoding(max_seq_len, embed_size)[tf.newaxis, :, :]

tests/test_tf_layers.py
@@ -168,7 +168,7 @@ def test_multi_head_attention():
         queries = tf.ones([2, 3, 4], dtype=tf.float32)
         keys = tf.reshape(tf.range(30, dtype=tf.float32), (2, 5, 3))
         rng = np.random.default_rng()
-        mask = tf.constant(rng.integers(0, 2, (2, 4, 3, 5), dtype=np.bool_))
+        mask = tf.constant(rng.integers(0, 2, (2, 3, 5), dtype=np.bool_))
         output1 = multi_head_attention(
             queries, keys, num_heads=4, head_dim=4, mask=mask, version="2.11"
         )