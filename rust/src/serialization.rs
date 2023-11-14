9746a7dddbd18089351c5b316d43636b9a07f6c5
massquantity <wdmjjxg@163.com>
2023-11-15 00:15:39 +0800

    [Rust] Model save & load (#407)

---
+++ rust/src/serialization.rs
@@ -0,0 +1,50 @@
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
+use std::path::Path;
+
+use flate2::{read::GzDecoder, write::GzEncoder, Compression};
+use pyo3::PyResult;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+
+pub fn save_model<T: Serialize>(
+    model: &T,
+    path: &str,
+    model_name: &str,
+    class_name: &str,
+) -> PyResult<()> {
+    let file_name = format!("{model_name}.gz");
+    let model_path = Path::new(path).join(file_name);
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(model_path.as_path())?;
+    let mut encoder = GzEncoder::new(file, Compression::default());
+    let model_bytes: Vec<u8> = bincode::serialize(model).unwrap();
+    encoder.write_all(&model_bytes)?;
+    encoder.finish()?;
+    println!(
+        "Save `{class_name}` model to `{}`",
+        model_path.canonicalize()?.display()
+    );
+    Ok(())
+}
+
+pub fn load_model<T: DeserializeOwned>(
+    path: &str,
+    model_name: &str,
+    class_name: &str,
+) -> PyResult<T> {
+    let file_name = format!("{model_name}.gz");
+    let model_path = Path::new(path).join(file_name);
+    let file = File::open(model_path.as_path())?;
+    let mut decoder = GzDecoder::new(file);
+    let mut model_bytes: Vec<u8> = Vec::new();
+    decoder.read_to_end(&mut model_bytes)?;
+    let model: T = bincode::deserialize(&model_bytes).unwrap();
+    println!(
+        "Load `{class_name}` model from `{}`",
+        model_path.canonicalize()?.display()
+    );
+    Ok(model)
+}
