9567bee2d24795f20f43cd083583af72104384ee
massquantity <wdmjjxg@163.com>
2023-11-26 19:59:30 +0800

    [Rust] Forward cosine computing (#419)

--- rust/src/similarities.rs
+++ rust/src/similarities.rs
@@ -3,25 +3,121 @@ use std::time::Instant;
 
 use fxhash::FxHashMap;
 use pyo3::PyResult;
+use rayon::prelude::*;
 
 use crate::sparse::{get_row, CsrMatrix};
+use crate::user_cf::CumValues;
 
 const MAX_BLOCK_SIZE: i64 = 200_000_000;
 
 pub(crate) fn compute_sum_squares(interactions: &CsrMatrix<i32, f32>, num: usize) -> Vec<f32> {
     (0..num)
-        .map(|i| match get_row(interactions, i) {
-            Some(row) => row.fold(0.0, |ss, (_, d)| ss + d * d),
-            None => 0.0,
-        })
+        .map(|i| get_row(interactions, i).map_or(0.0, |row| row.fold(0.0, |ss, (_, d)| ss + d * d)))
         .collect()
+}
+
+fn compute_cosine(prod: f32, sum_squ1: f32, sum_squ2: f32) -> f32 {
+    if prod == 0.0 || sum_squ1 == 0.0 || sum_squ2 == 0.0 {
+        0.0
+    } else {
+        let norm = sum_squ1.sqrt() * sum_squ2.sqrt();
+        prod / norm
+    }
+}
+
+#[derive(Debug)]
+struct SimVals {
+    x1: i32,
+    x2: i32,
+    prod: f32,
+    count: usize,
+    cosine: f32,
+}
+
+fn compute_row_sims(
+    interactions: &CsrMatrix<i32, f32>,
+    sum_squares: &[f32],
+    n_x: usize,
+    x1: usize,
+) -> Vec<SimVals> {
+    let (indices, indptr, data) = interactions.values();
+    let mut res = Vec::new();
+    for x2 in (x1 + 1)..n_x {
+        let mut i = indptr[x1];
+        let mut j = indptr[x2];
+        let end1 = indptr[x1 + 1];
+        let end2 = indptr[x2 + 1];
+        let mut prod = 0.0;
+        let mut count = 0;
+        while i < end1 && j < end2 {
+            let y1 = indices[i];
+            let y2 = indices[j];
+            match y1.cmp(&y2) {
+                Ordering::Less => i += 1,
+                Ordering::Greater => j += 1,
+                Ordering::Equal => {
+                    prod += data[i] * data[j];
+                    count += 1;
+                    i += 1;
+                    j += 1;
+                }
+            }
+        }
+        res.push(SimVals {
+            x1: x1 as i32,
+            x2: x2 as i32,
+            prod,
+            count,
+            cosine: compute_cosine(prod, sum_squares[x1], sum_squares[x2]),
+        });
+    }
+    res
+}
+
+pub(crate) fn forward_cosine(
+    interactions: &CsrMatrix<i32, f32>,
+    sum_squares: &[f32],
+    cum_values: &mut FxHashMap<i32, CumValues>,
+    n_x: usize,
+    min_common: usize,
+) -> PyResult<Vec<(i32, i32, f32)>> {
+    let start = Instant::now();
+    let sim_vals: Vec<SimVals> = (0..n_x)
+        .into_par_iter()
+        .flat_map(|x| compute_row_sims(interactions, sum_squares, n_x, x))
+        .collect();
+    let n_x = i32::try_from(n_x)?;
+    let mut cosine_sims: Vec<(i32, i32, f32)> = Vec::new();
+    for SimVals {
+        x1,
+        x2,
+        prod,
+        count,
+        cosine,
+    } in sim_vals
+    {
+        if count >= min_common {
+            cosine_sims.push((x1, x2, cosine));
+        }
+        if count > 0 {
+            let key = x1 * n_x + x2;
+            cum_values.insert(key, (x1, x2, prod, count));
+        }
+    }
+    let duration = start.elapsed();
+    println!(
+        "forward cosine sim: {} elapsed: {:.4?}",
+        cosine_sims.len(),
+        duration
+    );
+    Ok(cosine_sims)
 }
 
 /// Divide `n_x` into several blocks to avoid huge memory consumption.
 pub(crate) fn invert_cosine(
     interactions: &CsrMatrix<i32, f32>,
     sum_squares: &[f32],
-    cum_values: &mut FxHashMap<i32, (i32, i32, f32, usize)>,
+    cum_values: &mut FxHashMap<i32, CumValues>,
     n_x: usize,
     n_y: usize,
     min_common: usize,
@@ -56,20 +152,14 @@ pub(crate) fn invert_cosine(
             for x2 in (x1 + 1)..n_x {
                 let index = (x1 - block_start) * n_x + x2;
                 let prod = prods[index];
+                let count = counts[index];
                 let sq1 = sum_squares[x1];
                 let sq2 = sum_squares[x2];
                 let key = i32::try_from(x1 * n_x + x2)?;
                 let x1 = i32::try_from(x1)?;
                 let x2 = i32::try_from(x2)?;
-                let count = counts[index];
                 if count >= min_common {
-                    let cosine = if prod == 0.0 || sq1 == 0.0 || sq2 == 0.0 {
-                        0.0
-                    } else {
-                        let norm = sq1.sqrt() * sq2.sqrt();
-                        prod / norm
-                    };
-                    cosine_sims.push((x1, x2, cosine));
+                    cosine_sims.push((x1, x2, compute_cosine(prod, sq1, sq2)));
                 }
                 if count > 0 {
                     cum_values.insert(key, (x1, x2, prod, count));
@@ -79,7 +169,7 @@ pub(crate) fn invert_cosine(
     }
     let duration = start.elapsed();
     println!(
-        "cosine sim: {} elapsed: {:.4?}",
+        "invert cosine sim: {} elapsed: {:.4?}",
         cosine_sims.len(),
         duration
     );
