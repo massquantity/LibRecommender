7b5590a3c385946e94e77e296b3270f1990b806f
massquantity <wdmjjxg@163.com>
2024-03-03 23:46:00 +0800

    [Rust] Bump version to 0.0.5

--- rust/recfarm/__init__.py
+++ rust/recfarm/__init__.py
@@ -1,10 +1,13 @@
 from recfarm import recfarm
 from recfarm.recfarm import (
+    ItemCF,
     UserCF,
     __version__,
     build_consumed_unique,
+    load_item_cf,
     load_user_cf,
+    save_item_cf,
     save_user_cf,
 )
 
-__all__ = ["recfarm", "UserCF"]
+__all__ = ["recfarm", "UserCF", "ItemCF"]
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -17,8 +17,11 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 #[pymodule]
 fn recfarm(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<user_cf::PyUserCF>()?;
+    m.add_class::<item_cf::PyItemCF>()?;
     m.add_function(wrap_pyfunction!(user_cf::save, m)?)?;
     m.add_function(wrap_pyfunction!(user_cf::load, m)?)?;
+    m.add_function(wrap_pyfunction!(item_cf::save, m)?)?;
+    m.add_function(wrap_pyfunction!(item_cf::load, m)?)?;
     m.add_function(wrap_pyfunction!(utils::build_consumed, m)?)?;
     m.add("__version__", VERSION)?;
     Ok(())
