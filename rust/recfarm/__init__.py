8f3f2669d794faea5d2044ea8ce300be110596b4
massquantity <wdmjjxg@163.com>
2024-06-02 11:13:06 +0800

    [Rust] Swing module (#478)

--- rust/recfarm/__init__.py
+++ rust/recfarm/__init__.py
@@ -2,12 +2,15 @@ from recfarm import recfarm
 from recfarm.recfarm import (
     ItemCF,
     UserCF,
+    Swing,
     __version__,
     build_consumed_unique,
     load_item_cf,
     load_user_cf,
     save_item_cf,
     save_user_cf,
+    save_swing,
+    load_swing,
 )
 
-__all__ = ["recfarm", "UserCF", "ItemCF"]
+__all__ = ["recfarm", "UserCF", "ItemCF", "Swing"]
